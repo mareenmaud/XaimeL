af3f54ceb37ef027646ba871816346ecc75c5215
xavlat <xavlat@hotmail.fr>
2019-12-31 16:39:24 +0100

    ajout caroussel

--- src/main/webapp/app/service-profil-factice.service.ts
+++ src/main/webapp/app/service-profil-factice.service.ts
@@ -5,10 +5,10 @@ import { Injectable } from '@angular/core';
 })
 export class ServiceProfilFacticeService {
   tab = [
-    { nom: 'nom1', prenom: 'prenom1', messages: 'message1' },
-    { nom: 'nom2', prenom: 'prenom2', messages: 'message2' },
-    { nom: 'nom3', prenom: 'prenom3', messages: 'message3' },
-    { nom: 'nom4', prenom: 'prenom4', messages: 'message4' }
+    { nom: 'nom1', prenom: 'prenom1', messages: 'message1', photo: '/content/images/steph.jpg' },
+    { nom: 'nom2', prenom: 'prenom2', messages: 'message2', photo: '/content/images/mac.jpg' },
+    { nom: 'nom3', prenom: 'prenom3', messages: 'message3', photo: '/content/images/mac.jpg' },
+    { nom: 'nom4', prenom: 'prenom4', messages: 'message4', photo: '/content/images/steph.jpg' }
   ];
   constructor() {}
 }
