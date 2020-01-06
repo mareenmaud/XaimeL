319a56b6978f1979415494b84288396b2941d625
xavlat <xavlat@hotmail.fr>
2020-01-06 11:56:12 +0100

    front 2.2

--- src/main/webapp/app/page-profil-client/page-profil-client.component.ts
+++ src/main/webapp/app/page-profil-client/page-profil-client.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceProfilFacticeService } from 'app/service-profil-factice.service';
 
 @Component({
   selector: 'jhi-page-profil-client',
   templateUrl: './page-profil-client.component.html',
+  providers: [ServiceProfilFacticeService],
   styleUrls: ['./page-profil-client.component.scss']
 })
 export class PageProfilClientComponent implements OnInit {
-  constructor() {}
+  tab: any[];
+  constructor(serviceF: ServiceProfilFacticeService) {
+    this.tab = serviceF.tab;
+  }
 
   ngOnInit() {}
 }
