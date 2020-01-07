746559ba0bf3679d0c7e61e8e6e35a14cbeefa16
Zemmouri Samir <samir.zemmouri100@gmail.com>
2020-01-07 09:16:15 +0100

    first web services for create and read a conversaton

--- src/main/java/fr/inti/xml/repository/ConversationRepository.java
+++ src/main/java/fr/inti/xml/repository/ConversationRepository.java
@@ -1,9 +1,9 @@
 package fr.inti.xml.repository;
-import fr.inti.xml.domain.Conversation;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+    import fr.inti.xml.domain.Conversation;
+    import org.springframework.data.mongodb.repository.MongoRepository;
+    import org.springframework.stereotype.Repository;
 
-import java.util.List;
+    import java.util.List;
 
 
 /**
@@ -14,5 +14,6 @@ import java.util.List;
 public interface ConversationRepository extends MongoRepository<Conversation, String> {
 
     List<Conversation> findAllByIdUser1(String user_id1);
+    List<Conversation> findAllByIdUser2(String user_id2);
 
 }
